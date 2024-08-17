8f6c6b901c9c9ca3104312542d21b1b41eec968b
DedeSnrwn13 <sunarwandede13@gmail.com>
2024-08-17 11:05:17 +0700

    feat: js slider

---
+++ src/js/slider.js
@@ -0,0 +1,32 @@
+import { addClass, removeClass } from "./utils-class";
+
+const sliders = document.getElementsByClassName("slider");
+
+for (let index = 0; index < sliders.length; index++) {
+  const slider = sliders[index];
+
+  const items = slider.querySelectorAll(".slider .item");
+  const preview = slider.querySelector("div > .preview");
+
+  for (let item = 0; item < items.length; item++) {
+    const itemTrigger = items[item];
+
+    itemTrigger.addEventListener("click", function () {
+      const dataImg = this.attributes?.["data-img"]?.value;
+
+      for (
+        let eachItemTrigger = 0;
+        eachItemTrigger < items.length;
+        eachItemTrigger++
+      ) {
+        const triggeraNeedToRemoves = items[eachItemTrigger];
+
+        removeClass(triggeraNeedToRemoves, "selected");
+      }
+
+      addClass(itemTrigger, "selected");
+
+      preview.querySelector("img").setAttribute("src", dataImg);
+    });
+  }
+}
